321efaf626de5c5976299adb9ef10dbfc4700652
Ivan Tikhonov <kefeer@brokestream.com>
2014-09-25 17:45:49 +0600

    warn on possibly missed dash

--- tangle.py
+++ tangle.py
@@ -2,9 +2,15 @@ from sys import argv
 f=open(argv[1])
 m={'stdout':[]}
 cm=m['stdout']
+empty=True
+text=True
 while True:
 	l=f.readline()
 	if l=='': break
+	prev_empty=empty
+	empty=False
+	prev_text=text
+	text=False
 	if l.startswith('-'):
 		cm.append(l[1:])
 	elif l.startswith('='):
@@ -16,6 +22,16 @@ while True:
 	elif l.startswith('>'):
 		n=l[1:]
 		cm.append((n.strip(),n[:-len(n.lstrip())]))
+	elif l.strip()=='':
+		empty=True
+	else:
+		text=True
+		if (not prev_empty) and (not prev_text):
+			print('WARNING! This line probably should be code or have a blank line above:')
+			print(l)
+	if not empty and (prev_text and (not prev_empty) and (not text)):
+		print('WARNING! Line before that probably should be code or have a blank line after:')
+		print(l)
 def e(x,i=''):
 	if type(x) is tuple: return e(m[x[0]],i+x[1])
 	if type(x) is list: return ''.join([e(y,i) for y in x])
